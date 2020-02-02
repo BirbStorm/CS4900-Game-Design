fe99a982f1154acf4c9fc667fbe65299787a9909
Hampusli <hamlin-6@student.ltu.se>
2020-02-02 13:33:49 -0500

    Added offset to the camera

--- js/index2.js
+++ js/index2.js
@@ -50,6 +50,8 @@ function createCamera() {
   //creates initial camera
 	camera = new THREE.PerspectiveCamera( 45, container.clientWidth / container.clientHeight, 0.1, 100000 );
   camera.position.set( 0.25, -0.25, 10 );
+  camera.setViewOffset(container.clientWidth, container.clientHeight, 200, 0, container.clientWidth, container.clientHeight);
+  camera.updateProjectionMatrix();
   scene.add(camera);
 	camera.lookAt(scene.position);
 
