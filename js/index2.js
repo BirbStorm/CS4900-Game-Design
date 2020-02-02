8df884e56d1c3842d5f5dd066b12716d4e7a0fb5
Hampusli <hamlin-6@student.ltu.se>
2020-02-02 16:56:35 -0500

    Made a running animation for Pika

--- js/index2.js
+++ js/index2.js
@@ -50,7 +50,7 @@ function createCamera() {
   //creates initial camera
 	camera = new THREE.PerspectiveCamera( 45, container.clientWidth / container.clientHeight, 0.1, 100000 );
   camera.position.set( 0.25, -0.25, 10 );
-  camera.setViewOffset(container.clientWidth, container.clientHeight, 200, 0, container.clientWidth, container.clientHeight);
+  camera.setViewOffset(container.clientWidth, container.clientHeight, 200, 0, container.clientWidth, container.clientHeight); //Creates an offset to the camera so the model isn't in the way
   camera.updateProjectionMatrix();
   scene.add(camera);
 	camera.lookAt(scene.position);
