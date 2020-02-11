748b14cc1e8f7e27dd0b64f334d86d44306478b6
Hampusli <hamlin-6@student.ltu.se>
2020-02-10 22:08:02 -0500

    Added sprites for enemy information and a draft of a HUD

--- js/index2.js
+++ js/index2.js
@@ -4,13 +4,18 @@ import { Water } from './util/water.js'
 import * as controlsHelper from './util/controls.js'
 
 import { modelLoader } from './util/modelLoader.js'
+import { makeTextSprite } from './util/sprites.js'
 
 
 export let scene;
 export let isMouseDown;
 export let pika;
 export let terrain;
+let width;
+let height;
 let camera;
+let cameraHUD;
+let sceneHUD;
 let controls;
 let renderer;
 let keyboard = new THREEx.KeyboardState();
@@ -25,11 +30,24 @@ function main() {
   container = document.body;
   scene = new THREE.Scene();
 
+  // Create also a custom scene for HUD.
+  sceneHUD = new THREE.Scene();
+
+  // Create shortcuts for window size.
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+
 
   loadModels();
   createRenderer();
 
   camera = createCamera();
+
+  // Create the camera and set the viewport to match the screen dimensions.
+  var cameraHUD = new THREE.OrthographicCamera(-width/2, width/2, height/2, -height/2, 0.1, 100000 );
+  cameraHUD.position.z = 10;
+
+
   // scene.add(camera)
   console.log(renderer)
   controls = controlsHelper.createControls(camera, renderer);
@@ -39,6 +57,9 @@ function main() {
   createLights();
   createFloor();
   createSkyBox();
+  createHUD();
+  //createHealthBar();
+  createSprites();
 
   var axesHelper = new THREE.AxesHelper( 1 );
   scene.add( axesHelper );
@@ -129,6 +150,53 @@ function createRenderer() {
   document.body.appendChild( renderer.domElement );
 }
 
+function createSprites(){
+  var spritey = makeTextSprite( " World! ", 
+  { fontsize: 10, fontface: "Georgia", borderColor: {r:0, g:0, b:255, a:1.0} } );
+  spritey.position.set(-30,-10,0);
+  scene.add( spritey );
+}
+
+
+function createHUD(){
+
+
+  // We will use 2D canvas element to render our HUD.  
+  var hudCanvas = document.createElement('canvas');
+
+  // Again, set dimensions to fit the screen.
+  hudCanvas.width = width;
+  hudCanvas.height = height;
+
+  // Get 2D context and draw something supercool.
+  var hudBitmap = hudCanvas.getContext('2d');
+	hudBitmap.font = "Normal 40px Arial";
+  hudBitmap.textAlign = 'center';
+  hudBitmap.fillStyle = "rgba(245,245,245,0.75)";
+  hudBitmap.fillText('Initializing...', width / 2, height / 2);
+
+
+  
+ 
+ 
+	// Create texture from rendered graphics.
+	var hudTexture = new THREE.Texture(hudCanvas) 
+  hudTexture.needsUpdate = true;
+  
+  // Create HUD material.
+  var material = new THREE.MeshBasicMaterial( {map: hudTexture} );
+  material.transparent = true;
+
+  // Create plane to render the HUD. This plane fill the whole screen.
+  var planeGeometry = new THREE.PlaneGeometry( width, height );
+  var plane = new THREE.Mesh( planeGeometry, material );
+  sceneHUD.add( plane );
+}
+
+function createHealthBar(){
+  
+}
+
 function update() {
   const delta = clock.getDelta();
 
@@ -143,7 +211,10 @@ function animate() {
   requestAnimationFrame(animate)
   pika = scene.getObjectByName("pika")
   controlsHelper.updateControls()
+  //renderer.clear();
   renderer.render( scene, camera );
+  //renderer.clearDepth();
+  //renderer.render(sceneHUD, cameraHUD);
 }
 
 function onWindowResize() {
