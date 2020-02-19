85af76c5054aab2ecb58b29399baf089cdc56b44
Hampusli <hamlin-6@student.ltu.se>
2020-02-19 15:27:18 -0500

    Changed music volume and added correct button images

--- js/util/audio.js
+++ js/util/audio.js
@@ -1,13 +1,17 @@
 let mute = false;
 let music = document.getElementById('music');
+let muteButton = document.getElementById('muteButton');
 
+music.volume = 0.1;
 function toggleMute(){
     if(mute){
         mute = false;
         music.play();
+        muteButton.src = "/assets/textures/unmute.png";
     }
     else if(!mute){
         mute = true;
         music.pause();
+        muteButton.src = "/assets/textures/mute.png";
     }
 }
