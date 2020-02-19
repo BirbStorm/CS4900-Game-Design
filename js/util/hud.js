1f85af3c5b4152350530cf48919c6d4065b2243c
CalebPierce <capierce@valdosta.edu>
2020-02-18 20:49:33 -0500

    added loseHealth in the hud.js

---
+++ js/util/hud.js
@@ -0,0 +1,9 @@
+function loseHealth(damage){
+    /* Works somthing like this
+
+    use the class method for taking damage
+    let pxValue = max player hp/400; //one px's worth of health
+    let x = Math.floor(current health /pxValue);
+    document.getElementById("hpbar").width = x;
+    */
+}
