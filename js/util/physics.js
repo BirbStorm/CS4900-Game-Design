b0f874fea67baabdac8ceb8b94bc1d6c7f7c1f81
Hampusli <hamlin-6@student.ltu.se>
2020-04-15 08:55:17 -0400

    Added a fade out function for sound

--- js/util/physics.js
+++ js/util/physics.js
@@ -56,8 +56,8 @@ export function initPhysics() {
         if(!playerLanded){
             playerLanded = true;
             activateAllActions();
-            flying.pause();
             music.play();
+            getSoundAndFadeAudio('flying');
         }
     }
     
@@ -186,3 +186,25 @@ export function updatePhysics( deltaTime ) {
 
 }
 
+
+function getSoundAndFadeAudio (audiosnippetId) {
+
+    var sound = document.getElementById(audiosnippetId);
+
+    // Set the point in playback that fadeout begins. This is for a 2 second fade out.
+    var fadePoint = sound.duration - 1; 
+
+    var fadeAudio = setInterval(function () {
+
+        // Only fade if past the fade out point or not at zero already
+        if ((sound.currentTime >= fadePoint) && (sound.volume != 0.0)) {
+            sound.volume -= 0.1;
+        }
+        // When volume at zero stop all the intervalling
+        if (sound.volume === 0.0) {
+            clearInterval(fadeAudio);
+            sound.pause();
+        }
+    }, 200);
+
+}
